bf646c7612af7162beaf4c19caa592ca16ee97a4
Leobruno <leobrunofreire10@gmail.com>
2025-09-25 21:13:13 -0300

    Repositórios novos

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,38 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main class="page">
+      <header class="page-header">
+        <img src="../images/Vector.png" alt="logo" class="page-logo" />
+      </header>
+      <div class="author">
+        <div class="author-avatar-container">
+          <img class="author-image" src="../images/Avatar.png" alt="Autor" />
+          <button class="author-avatar-edit-button" type="button"></button>
+        </div>
+        <div class="author-info">
+          <div class="author-name-wrapper">
+            <h1 id="profileName" class="author-name">Jacques Cousteau</h1>
+            <button id="openModalBtn" class="author-edit"></button>
+          </div>
+          <p class="author-subtitle" id="profileTitle">Explorador</p>
+        </div>
+        <button id="openPlaceModalBtn" class="author-add"></button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <section class="elements">
+        <template id="card-template">
+          <div class="element">
+            <img class="element-image" src="" alt="" />
+            <button class="element-delete-btn"></button>
+            <p class="element-image-title">
+              <span></span>
+              <button class="element-image-like"></button>
+            </p>
+          </div>
+        </template>
+      </section>
+      <footer class="footer">&copy; 2025 Around The U.S.</footer>
+      </main>
     </>
   )
 }
